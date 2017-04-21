32a76c27d95cc22202bebdb5d9f930e63e3fa198
pac-yan <pyan@stuy.edu>
2017-04-21 08:01:27 -0400

    Magic

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,1 +1,19 @@
-
+/*
+Class HelpDesk
+*/
+public class HelpDesk{
+    private ArrayPriorityQueue<Ticket> ticketQ;
+    private ArrayList<Ticket> SolvedTickets;
+    public HelpDesk(){
+	ticketQ = new ArrayPriorityQueue<Ticket>();
+	SolvedTickets = new ArrayList<Ticket>();
+    }
+    private void createTicket(String name, int VIP, String problem){
+    }
+    private void place(Ticket x){
+    }
+    private boolean 
+    private void solveNew(){
+    }
+    private void solveOld(Ticket y){
+    }
--- Ticket.java
+++ Ticket.java
@@ -1,1 +1,37 @@
-
+/*
+Class Ticket
+*/
+public class Ticket{
+    private int ID;
+    private int VIP;
+    private String problem;
+    private boolean solved;
+    private String name;
+    private String solution;
+    public Ticket(int id, int vip, String prob, String nombre){
+	ID = id;
+	VIP = vip;
+	problem = prob;
+	solved = false;
+	name = nombre;
+	solution = "";
+    }
+    public int getID(){
+	return ID;
+    }
+    public int getVIP(){
+	return VIP;
+    }
+    public String getProblem(){
+	return problem;
+    }
+    public boolean getSolved(){
+	return solved;
+    }
+    public String getName(){
+	return name;
+    }
+    public String getSolution(){
+	return solution;
+    }
+}
