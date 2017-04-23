0da9a228c68a1d377e4488101a5095cb17d9c4f5
Daniel Ju <cju@stuy.edu>
2017-04-23 00:09:32 -0400

    did some work on HelpDesk

--- HelpDesk.java
+++ HelpDesk.java
@@ -9,11 +9,28 @@ public class HelpDesk{
 	SolvedTickets = new ArrayList<Ticket>();
     }
     private void createTicket(String name, int VIP, String problem){
+	int a = (int) (Math.random() * 1000); 
+	ticket newTicket = new ticket(a, VIP, problem, name);
+	System.out.println("Your ticket number is: " + a + "! Don't forget it!");
     }
     private void place(Ticket x){
+	ticketQ.add(x, x.VIP);
     }
-    private boolean 
+    private void solveNext(){
+	if (SolvedTickets.contains(ticketQ.peekMin())){
+	    solveOld();
+	}
+	else{
+	    solveNew();
+	}
+    }
     private void solveNew(){
+	ticket workOnNow = ticketQ.peekMin();
+	//Deal with problem here
+	ticketQ.removeMin();
+	SolvedTickets.add(workOnNow);
     }
     private void solveOld(Ticket y){
+	
     }
+}
---
+++ PriorityQueue.java
@@ -0,0 +1,7 @@
+public interface PriorityQueue
+{
+    void add( Object x );
+    boolean isEmpty();
+    Object peekMin();
+    Object removeMin();
+}
